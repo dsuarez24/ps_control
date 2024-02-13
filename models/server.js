f7644762d8871535b6fe02b62a59c6efba1244db
dsuarez24 <dsuarez-2022233@kinal.edu.gt>
2024-02-13 16:35:25 -0600

    Se creo el modelo de alumno

---
+++ models/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuarioPath = ''
+        
+
+    }
+
+}
