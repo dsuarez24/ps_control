9c546fab76c9e535c59f166d2330deb609815ae3
dsuarez24 <dsuarez-2022233@kinal.edu.gt>
2024-02-13 16:24:12 -0600

    Se creo el modelo de roles y el config

---
+++ models/role.js
@@ -0,0 +1,11 @@
+const {schema, model, Schema}= require('mongoose');
+
+const RoleSchema = Schema({
+    role:{
+        type: String,
+        required: [true, 'El role es obligatorio']
+    }
+
+});
+
+module.exports = model ('Role', RoleSchema);
